dcaa4fbaa87b38eb089127aa1866f6ef871c0968
Ryan Oberholzer <ryanoberholzer@ryan-oberholzers-macbook-pro.local>
2009-10-07 16:49:55 +0200

    initial commit

---
+++ Rakefile
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('easy_roles', '0.1.0') do |p|
+  p.description    = "Easy role authorization in rails"
+  p.url            = "http://github.com/platform45/easy_roles"
+  p.author         = "Platform45"
+  p.email          = "ryan@platform45.com"
+  p.ignore_pattern = ["tmp/*", "script/*"]
+  p.development_dependencies = []
+end
---
+++ lib/easy_roles.rb
@@ -0,0 +1,56 @@
+module EasyRoles
+  def self.included(base)
+    base.extend ClassMethods
+    
+    base.send :alias_method_chain, :method_missing, :roles
+  end
+  
+  module ClassMethods
+    def easy_roles(name)
+      serialize name.to_sym, Array
+      before_validation_on_create :make_default_roles
+      
+      class_eval <<-EOC
+        def has_role?(role)
+          #{name}.include?(role)
+        end
+
+        def add_role(role)
+          self.#{name} << role
+        end
+
+        def remove_role(role)
+          self.#{name}.delete(role)
+        end
+        
+        def clear_roles
+          self.#{name} = []
+        end
+        
+        def make_default_roles
+          clear_roles if roles.nil?
+        end
+        
+        private :make_default_roles
+      EOC
+    end
+  end
+  
+  def method_missing_with_roles(method_id, *args, &block)
+    match = method_id.to_s.match(/^is_(\w+)[?]$/)
+    if match && respond_to?('has_role?')
+      class_eval <<-EOC
+        def is_#{match[1]}?
+          send :has_role?, '#{match[1]}'
+        end
+      EOC
+      send "is_#{match[1]}?"
+    else
+      method_missing_without_roles(method_id, *args, &block)
+    end
+  end
+end
+
+class ActiveRecord::Base
+  include EasyRoles
+end
