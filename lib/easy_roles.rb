809b76b1fce83f05e1adbad144a9978f25ec1cdf
Ryan Oberholzer <ryan@platform45.com>
2010-04-05 23:41:27 +0200

    Updated for rails 3 support

--- lib/easy_roles.rb
+++ lib/easy_roles.rb
@@ -11,7 +11,7 @@ module EasyRoles
       
       if options[:method] == :serialize
         serialize name.to_sym, Array
-        before_validation_on_create :make_default_roles
+        respond_to?(:before_validation_on_create) ? before_validation_on_create(:make_default_roles) : before_validation(:make_default_roles, :on => :create)
       
         class_eval <<-EOC
           def has_role?(role)
