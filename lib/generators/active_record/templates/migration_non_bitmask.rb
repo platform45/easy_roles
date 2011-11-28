e9fe85a15d16e88feac93e00691b02d0ce271914
Daniel Davey <whoisdanieldavey@gmail.com>
2011-11-28 11:36:26 +0200

    Cleaned up generators.

--- lib/generators/active_record/templates/migration_non_bitmask.rb
+++ lib/generators/active_record/templates/migration_non_bitmask.rb
@@ -2,9 +2,9 @@ class AddEasyRolesTo<%= table_name.camelize %> < ActiveRecord::Migration
   def change
     change_table :<%= table_name %> do |t|
       t.string :<%= self.role_col %>, default: '--- []'
-      <% if options.add_index %>
-        t.index :<%= self.role_col %>
-      <% end %>
+     <% if options.add_index -%>
+      t.index :<%= self.role_col %>
+     <% end -%>
     end
   end
 end
