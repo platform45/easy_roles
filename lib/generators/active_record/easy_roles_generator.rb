b3cc1d6d4477f55c28f4d37889436f035d5f5a7f
Victor Velazquez <victor.velazquez@crowdint.com>
2015-04-02 13:36:16 -0600

    Add missing ruby extension to migrations
    When the generator create the migrations those migrations are
    without a ruby extension .rb

--- lib/generators/active_record/easy_roles_generator.rb
+++ lib/generators/active_record/easy_roles_generator.rb
@@ -18,9 +18,9 @@ module ActiveRecord
 
       def copy_easy_roles_migration
         if options.use_bitmask_method
-          migration_template 'migration_bitmask.rb', "db/migrate/add_bitmask_roles_to_#{table_name}"
+          migration_template 'migration_bitmask.rb', "db/migrate/add_bitmask_roles_to_#{table_name}.rb"
         else
-          migration_template 'migration_non_bitmask.rb', "db/migrate/add_easy_roles_to_#{table_name}"
+          migration_template 'migration_non_bitmask.rb', "db/migrate/add_easy_roles_to_#{table_name}.rb"
         end
       end
 
